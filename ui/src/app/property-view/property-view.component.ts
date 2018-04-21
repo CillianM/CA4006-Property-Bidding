0080f5c497870000f9e92bcb0ca38a2a6888450e
Cillian <cillianmcneill@gmail.com>
2018-04-21 23:36:53 +0100

    Tomcat fixes for navbar & sounds

--- ui/src/app/property-view/property-view.component.ts
+++ ui/src/app/property-view/property-view.component.ts
@@ -225,7 +225,7 @@ export class PropertyViewComponent implements OnInit {
         var json = JSON.parse(message.data);
         console.log(json);
         var audio = new Audio();
-        audio.src = "../../../assets/notification.mp3";
+        audio.src = "/ui/assets/notification.mp3";
         audio.load();
         audio.play();
         let newList = [];
